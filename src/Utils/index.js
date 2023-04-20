545c5ec43e55d4536c724d964d2d3f5453809c07
Joshua Jakab <joshua.jakab@gmail.com>
2023-04-20 15:04:06 -0400

    changed api

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const apiInstance = axios.create({
     //baseURL: 'https://us-central1-green-mountain-synergy.cloudfunctions.net/api'
-    baseURL:'http://localhost:5001/joshuaforvermont/us-central1/api'
+    baseURL:'https://us-central1-joshuaforvermont.cloudfunctions.net/api'
 })
